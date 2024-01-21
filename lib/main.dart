7ed4724ee10fe5256c72ffd038a67260fd74e021
sagar <sagar123@gmail.com>
2024-01-21 13:21:07 +0530

    day_2_completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_test_pro/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
- /* @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          child: Center(
-            child: Text("Hello, Sagar"),
-          ),
-        ),
-      ),
-    );
-  }*/
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Center(
-          child: Container(
-              child: Text("Hello, Sagar"),
-            ),
-        ),
-        );
+    return  MaterialApp(
+        home: HomePage());
   }
 }
 
