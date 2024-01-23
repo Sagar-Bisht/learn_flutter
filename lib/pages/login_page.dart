f825f11b1d182bd81cf11e717c34c1974dc4a091
sagar <sagar123@gmail.com>
2024-01-23 23:16:09 +0530

    day 4 complte

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,16 +5,61 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-          child: Text("Login Page",
+    return Material(
+        color: Colors.blue,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50.0,
+            ),
+            Image.asset("assets/images/image.png",
+                fit: BoxFit.scaleDown, width: 500, height: 300),
+            SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "Welcome Sir",
               style: TextStyle(
                   fontSize: 20,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Your Name", labelText: "Name"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Your Password",
+                      labelText: "Password",
+                    ),
+                  )
+                ],
               ),
-          ),
-      ),
-    );
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("Hello sagar");
+              },
+              child: Text("Login"),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.black) ,
+                backgroundColor: MaterialStatePropertyAll(Colors.white)
+              ),
+            )
+          ],
+        ));
   }
 }
