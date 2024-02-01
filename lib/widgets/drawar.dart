d780e041ef851d44401711ff19652ee70a4f8905
sagar <sagar123@gmail.com>
2024-02-01 23:51:03 +0530

    learning drawer_2

--- lib/widgets/drawar.dart
+++ lib/widgets/drawar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -27,7 +28,23 @@ class MyDrawer extends StatelessWidget {
                       fit: BoxFit.contain, // Set the scale type here
                     ),
                   ),
-                ))
+                )
+            ) ,
+            ListTile(
+              leading: Icon(CupertinoIcons.home ,color: Colors.white , size: 30) ,
+              title: Text("Home" , style: TextStyle(color: Colors.white , fontSize: 16)) ,
+                trailing: Icon(Icons.arrow_forward , color: Colors.white)
+            ) ,
+            ListTile(
+                leading: Icon(CupertinoIcons.profile_circled ,color: Colors.white , size: 30) ,
+                title: Text("Profile" , style: TextStyle(color: Colors.white , fontSize: 16)) ,
+                trailing: Icon(Icons.arrow_forward , color: Colors.white)
+            ) ,
+            ListTile(
+                leading: Icon(CupertinoIcons.settings ,color: Colors.white , size: 30) ,
+                title: Text("Settings" , style: TextStyle(color: Colors.white , fontSize: 16)) ,
+                trailing: Icon(Icons.arrow_forward , color: Colors.white)
+            ) ,
           ],
         ),
       ),
